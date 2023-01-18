70300b53865286383233c4aace4fd31195bd7050
KYankee6 <highestbright98@naver.com>
2023-01-19 00:15:08 +0900

    sleepy

---
+++ lib/repositories/userRepository.dart
@@ -0,0 +1,38 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class UserRepository{
+  final FirebaseAuth _firebaseAuth;
+  final FirebaseFirestore _firestore;
+
+
+  UserRepository({
+    FirebaseAuth firebaseAuth,
+    FirebaseFirestore firestore
+}): _firebaseAuth = firebaseAuth ?? FirebaseAuth.instance,
+  _firestore = firestore ?? FirebaseFirestore.instance;
+
+  Future<void> signInWithEmail(String email, String password) {
+    _firebaseAuth.signInWithEmailAndPassword(email: email, password: password);
+  }
+
+  Future<bool> isFirstTime(String userId) async {
+    bool exist;
+    await FirebaseFirestore.instance.collection("users")
+        .doc(userId).get()
+        .then((user) {
+      exist = user.exists;
+    });
+
+    return exist;
+  }
+
+  Future<void> signUpWithEmail(String email, String password) async {
+    print(_firebaseAuth);
+    return await _firebaseAuth
+        .createUserWithEmailAndPassword{email: email, password: password}
+
+
+  }
+
+}
