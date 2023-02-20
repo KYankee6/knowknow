d21be633305986fcf97347016c3485832b27603c
KYankee6 <highestbright98@naver.com>
2023-02-21 00:46:50 +0900

    sleepy

---
+++ lib/bloc/login/login_state.dart
@@ -0,0 +1,86 @@
+part of 'login_bloc.dart';
+
+@immutable
+abstract class LoginState {
+  final bool isEmailValid;
+  final bool isPasswordValid;
+  final bool isSubmitting;
+  final bool isSuccess;
+  final bool isFailure;
+
+  bool get isFormValid => isEmailValid && isPasswordValid;
+
+  LoginState({required this.isEmailValid,
+    required this.isPasswordValid,
+    required this.isSubmitting,
+    required this.isSuccess,
+    required this.isFailure});
+
+  //initial state
+  factory LoginState.empty() {
+    return LoginState(
+      isEmailValid: true,
+      isPasswordValid: true,
+      isSubmitting: false,
+      isFailure: false,
+      isSuccess: false,
+    );
+  }
+
+  factory LoginState.loading() {
+    return LoginState(
+      isEmailValid: true,
+      isPasswordValid: true,
+      isSubmitting: true,
+      isFailure: false,
+      isSuccess: false,
+    );
+  }
+
+  factory LoginState.failure() {
+    return LoginState(
+      isEmailValid: true,
+      isPasswordValid: true,
+      isSubmitting: false,
+      isFailure: true,
+      isSuccess: false,
+    );
+  }
+
+  factory LoginState.success() {
+    return LoginState(
+      isEmailValid: true,
+      isPasswordValid: true,
+      isSubmitting: false,
+      isFailure: false,
+      isSuccess: true,
+    );
+  }
+
+  LoginState update({
+    final bool? isEmailValid;
+    final bool isPasswordValid;
+  }) {
+    return copyWith(
+      isEmailValid: isEmailValid,
+      isPasswordValid: isPasswordValid,
+      isSubmitting: false,
+      isFailure: false,
+      isSuccess: true,
+    );
+  }
+
+  LoginState copyWith(
+      {bool? isEmailValid, required bool isPasswordValid, required bool isSubmitEnabled, required bool isSubmitting, required bool isFailure, required bool isSuccess}) {
+    return LoginState(
+      isEmailValid: isEmailValid ?? this.isEmailValid,
+      isPasswordValid: isPasswordValid ?? this.isPasswordValid,
+      isSubmitting: isSubmitting ?? this.isSubmitting,
+      isSuccess: isSuccess ?? this.isSuccess,
+      isFailure: isFailure ?? this.isFailure,
+    );
+  }
+
+}
+
+class LoginInitial extends LoginState {}
